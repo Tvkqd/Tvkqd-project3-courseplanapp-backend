4ec333ea72582416c0128669560408cb04fd289a
Tuyen Vu <tuyenvu1108@gmail.com>
2022-10-03 09:44:59 -0500

    update deploy file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 var corsOptions = {
-//  origin: "http://localhost/course-t1"
+//  origin: "http://localhost/schedule-t1"
   origin: "http://localhost:8080"
 };
 app.use(cors(corsOptions));
