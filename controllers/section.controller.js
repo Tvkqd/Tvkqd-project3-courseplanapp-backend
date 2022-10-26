cd990eb27c7a77b96342df86f8c3ba7fd3e74a41
Nate Bellcock <nate.bellcock@eagles.oc.edu>
2022-10-26 09:13:56 -0500

    added Section.controllor.js

---
+++ controllers/section.controller.js
@@ -0,0 +1,134 @@
+const db = require("../models");
+const Section = db.section;
+const Op = db.Sequelize.Op;
+// Create and Save 
+exports.create = (req, res) => {
+        // Valcourse_numberate request
+    if (!req.body.course_number) {
+        res.status(400).send({
+        message: "Content can not be empty!"
+        });
+        return;
+    }
+    // Create
+    const section = {
+        number: req.body.number,
+        course_id: req.body.course_id,
+        semaster: req.body.semaster_id
+    };
+    // Save in the database
+    Section.create(section)
+        .then(data => {
+        res.send(data);
+        })
+        .catch(err => {
+        res.status(500).send({
+            message:
+            err.message || "Some error occurred while creating the Course."
+        });
+        });
+};
+
+// Retrieve all courses from the database.
+exports.findAll = (req, res) => {
+    const course_number = req.query.course_number;
+    var condition =course_number ? {course_number: { [Op.like]: `%$course_number}%` } } : null;
+    Section.findAll({ where: condition,
+      include: [{
+      model: User,
+      required: true
+     }]})
+        .then(data => {
+        res.send(data);
+        })
+        .catch(err => {
+        res.status(500).send({
+            message:
+            err.message || "Some error occurred while retrieving courses."
+        });
+      });
+};
+
+
+// Retrieve all courses from the database with a specific title.
+exports.findName = (req, res) => {
+  const name = req.params.name;
+  Section.findAll({ where: {name: {[Op.substring]: name}} })
+      .then(data => {
+      res.send(data);
+      })
+      .catch(err => {
+      res.status(500).send({
+          message:
+          err.message || "Some error occurred while retrieving courses."
+      });
+    });
+};
+
+// Find a single course with a course_number
+exports.findOne = (req, res) => {
+    const course_number = req.params.course_number;
+    Section.findByPk(course_number)
+      .then(data => {
+        if (data) {
+          res.send(data);
+        } else {
+          res.status(404).send({
+            message: `Cannot find Course with course_number=${course_number}.`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving Course with course_number=" + course_number
+        });
+      });
+};
+// Update by the id in the request
+exports.update = (req, res) => {
+    const id = req.params.id;
+    Section.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Course was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update Course with id=${id}. Maybe Course was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating Course with id=" + id
+        });
+      });
+};
+// Delete with the specified course_number in the request
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    Section.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Course was deleted successfully!"
+          });
+        } else {
+          console.log("error: " + num)
+          res.send({
+            message: `Cannot delete Course with id=${id}. Maybe Course was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        console.log("error: " + err)
+        res.status(500).send({
+          message: "Could not delete Course with id=" + id
+        });
+      });
+};
