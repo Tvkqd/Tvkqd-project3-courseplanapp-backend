7b0e51aae4f93b08ba9fb6339e5cd3f725f7745b
ThatGremlin <ben.johnson@eagles.oc.edu>
2022-10-27 18:32:58 -0500

    Changed facultySection name
    Changed copy pasted faculty to facultySection

--- routes/faculty.section.js
+++ routes/faculty.section.js
@@ -1,16 +1,16 @@
 module.exports = app => {
-    const faculty = require("../controllers/faculty.controller.js");
+    const facultySection = require("../controllers/faculty.controller.js");
     var router = require("express").Router();
     // Create 
-    router.post("/", faculty.create);
+    router.post("/", facultySection.create);
     // Retrieve all 
-    router.get("/", faculty.findAll);
-    // Retrieve a single faculty with id
-    router.get("/:id", faculty.findOne);
+    router.get("/", facultySection.findAll);
+    // Retrieve a single facultySection with id
+    router.get("/:id", facultySection.findOne);
     // Update with id
-    router.put("/:id", faculty.update);
+    router.put("/:id", facultySection.update);
     // Delete with id
-    router.delete("/:id", faculty.delete);
-    //Changed to schedule to build the api path to match the AWS server. Making router file unique by adding /courses
-    app.use('/schedule-t1/facultySECTION', router);
+    router.delete("/:id", facultySection.delete);
+    //Changed to scheduleSection to build the api path to match the AWS server. Making router file unique by adding /courses
+    app.use('/schedule-t1/facultySection', router);
   };
