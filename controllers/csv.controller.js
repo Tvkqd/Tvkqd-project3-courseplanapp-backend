c578f4ef58e7036c591f9c512378f296eb4eb2a7
Tuyen Vu <tuyenvu1108@gmail.com>
2022-11-02 09:17:57 -0500

    Section upload fixed writing to database error

--- controllers/csv.controller.js
+++ controllers/csv.controller.js
@@ -18,13 +18,11 @@ const uploadSections = async (req, res) => {
     let sectionTimes = [];
     let path = __basedir + "/resources/static/assets/uploads/sections/" + req.file.filename;
 
-    fs.createReadStream(path)
-      .pipe(csv.parse({ headers: ["Term","Synonym","UG/GR","Section Number","Subject","Course #","Section #","Crs Level","Course Type","Reg Restrictions","Bldg1","Room1","Section Title","Faculty Name (LFM)","Faculty Name 2 (LFM)","Sec Start Date","Meeting Start Date","Sec End Date","Meeting End Date","Start Time1","End Time1","Days","Sec Num Of Weeks ","Min Cred","Max Cred","Capacity","Enr Count","Wait","Depts","Divisions","College","Faculty Last Name","Faculty First Name","Instr Method","Sun","Mon","Tue","Wed","Thu","Fri","Sat","SEC.XLIST","SEC.FEE","Primary Section ","SEC.COMMENTS","SEC.PRINTED.COMMENTS","Academic Year","Term Sort No","Only Pass/NoPass","Allow Pass/NoPass","Bldg2","Room","Start Date","End Date","Start Time2","Start Time 24hr","End Time2","End Time 24hr"], renameHeaders:true }))
-      .on("error", (error) => {
-        throw error.message;
-      })
-      .on("data", async (row) => {
+    new Promise((resolve, reject) => {
+      const promises = [];
+      async function processData(row){
         // Foreign key courseID
+        console.log(row);
         const [course, createdCourse] = await Course.findOrCreate({
           where: { dept: row["Depts"], subject: row["Subject"], courseNum:row["Course #"]},
         });
@@ -49,25 +47,91 @@ const uploadSections = async (req, res) => {
         console.log(room.id);
         console.log(createdRoom);
         // Read data for sectionTime table
-        let newRow = {startDate:row["Sec Start Date"], endDate:row["Sec End Date"], startTime:row["Start Time1"], endTime:row["End Time1"], dayWeek:row["Days"], numWeek:row["Sec Num Of Weeks "], capacity:row["Capacity"], instrMethod:row["Instr Method"], sectionId:section.id, roomId:room.id}
+        let newRow = {startDate:row["Sec Start Date"], endDate:row["Sec End Date"], startTime:row["Start Time 24hr"], endTime:row["End Time 24hr"], dayWeek:row["Days"], numWeek:row["Sec Num Of Weeks "], capacity:row["Capacity"], instrMethod:row["Instr Method"], sectionId:section.id, roomId:room.id}
         console.log(newRow);
         sectionTimes.push(newRow);
-      })
-      .on("end", async () => {
-        await SectionTime.bulkCreate(sectionTimes)
+        console.log(sectionTimes);
+      }
+      fs.createReadStream(path)
+        .pipe(csv.parse({ headers: ["Term","Synonym","UG/GR","Section Number","Subject","Course #","Section #","Crs Level","Course Type","Reg Restrictions","Bldg1","Room1","Section Title","Faculty Name (LFM)","Faculty Name 2 (LFM)","Sec Start Date","Meeting Start Date","Sec End Date","Meeting End Date","Start Time1","End Time1","Days","Sec Num Of Weeks ","Min Cred","Max Cred","Capacity","Enr Count","Wait","Depts","Divisions","College","Faculty Last Name","Faculty First Name","Instr Method","Sun","Mon","Tue","Wed","Thu","Fri","Sat","SEC.XLIST","SEC.FEE","Primary Section ","SEC.COMMENTS","SEC.PRINTED.COMMENTS","Academic Year","Term Sort No","Only Pass/NoPass","Allow Pass/NoPass","Bldg2","Room","Start Date","End Date","Start Time2","Start Time 24hr","End Time2","End Time 24hr"], renameHeaders:true }))
+        .on("error", (error) => {
+          throw error.message;
+        })
+        .on("data", row => promises.push(processData(row)))
+        .on("error", reject)
+        .on("end", async () => {
+          await Promise.all(promises);
+          resolve();
+          console.log(sectionTimes)
+          await SectionTime.bulkCreate(sectionTimes)
           .then(() => {
-            res.status(200).send({
-              message:
-                "Uploaded the file successfully: " + req.file.originalname,
-            });
-          })
+                    res.status(200).send({
+                      message:
+                        "Uploaded the file successfully: " + req.file.originalname,
+                    });
+                  })
           .catch((error) => {
-            res.status(500).send({
-              message: "Fail to import data into database!",
-              error: error.message,
-            });
-          });
-      });
+                    res.status(500).send({
+                      message: "Fail to import data into database!",
+                      error: error.message,
+                    });
+                  });
+        });
+    });
+    // fs.createReadStream(path)
+    //   .pipe(csv.parse({ headers: ["Term","Synonym","UG/GR","Section Number","Subject","Course #","Section #","Crs Level","Course Type","Reg Restrictions","Bldg1","Room1","Section Title","Faculty Name (LFM)","Faculty Name 2 (LFM)","Sec Start Date","Meeting Start Date","Sec End Date","Meeting End Date","Start Time1","End Time1","Days","Sec Num Of Weeks ","Min Cred","Max Cred","Capacity","Enr Count","Wait","Depts","Divisions","College","Faculty Last Name","Faculty First Name","Instr Method","Sun","Mon","Tue","Wed","Thu","Fri","Sat","SEC.XLIST","SEC.FEE","Primary Section ","SEC.COMMENTS","SEC.PRINTED.COMMENTS","Academic Year","Term Sort No","Only Pass/NoPass","Allow Pass/NoPass","Bldg2","Room","Start Date","End Date","Start Time2","Start Time 24hr","End Time2","End Time 24hr"], renameHeaders:true }))
+    //   .on("error", (error) => {
+    //     throw error.message;
+    //   })
+    //   .on("data", async (row) => {
+    //     // Foreign key courseID
+    //     console.log(row);
+    //     const [course, createdCourse] = await Course.findOrCreate({
+    //       where: { dept: row["Depts"], subject: row["Subject"], courseNum:row["Course #"]},
+    //     });
+    //     console.log(course.id);
+    //     console.log(createdCourse);
+    //     // Foreign key semesterID
+    //     const [semester, createdSemester] = await Semester.findOrCreate({
+    //       where: { code: row["Term"]},
+    //     });
+    //     console.log(semester.id);
+    //     console.log(createdSemester);
+    //     // Foreign key sectionID
+    //     const [section, createdSection] = await Section.findOrCreate({
+    //       where: { number: row["Section Number"], subject: row["Subject"], courseNum:row["Course #"], sectionNum: row["Section #"], title: row["Section Title"], courseId: course.id, semesterId: semester.id },
+    //     });
+    //     console.log(section.id);
+    //     console.log(createdSection);
+    //     // Foreign key roomID
+    //     const [room, createdRoom] = await Room.findOrCreate({
+    //       where: {bldg: row["Bldg1"], name: row["Room1"] },
+    //     });
+    //     console.log(room.id);
+    //     console.log(createdRoom);
+    //     // Read data for sectionTime table
+    //     let newRow = {startDate:row["Sec Start Date"], endDate:row["Sec End Date"], startTime:row["Start Time 24hr"], endTime:row["End Time 24hr"], dayWeek:row["Days"], numWeek:row["Sec Num Of Weeks "], capacity:row["Capacity"], instrMethod:row["Instr Method"], sectionId:section.id, roomId:room.id}
+    //     console.log(newRow);
+    //     sectionTimes.push(newRow);
+    //     console.log(sectionTimes);
+    //   })
+      
+    //   .on("end", async () => {
+    //     console.log(sectionTimes)
+    //     await SectionTime.bulkCreate(sectionTimes)
+    //       .then(() => {
+    //         res.status(200).send({
+    //           message:
+    //             "Uploaded the file successfully: " + req.file.originalname,
+    //         });
+    //       })
+    //       .catch((error) => {
+    //         res.status(500).send({
+    //           message: "Fail to import data into database!",
+    //           error: error.message,
+    //         });
+      //     });
+      // });
   } catch (error) {
     console.log(error);
     res.status(500).send({
@@ -94,9 +158,9 @@ const uploadCourses = async (req, res) => {
       })
       .on("data", (row) => {
         let newRow = {dept:row["Department"], course_number:row["Course"], level:row["Course Levels"], hours:row["Min Credits"], name:row["Short Title"], description:row["Description"]}
-        //console.log(newRow);
+        console.log(newRow);
         courses.push(newRow);
-        //console.log(courses);
+        console.log(courses);
       })
       .on("end", () => {
         Course.bulkCreate(courses)
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -3,7 +3,7 @@ module.exports = app => {
     var router = require("express").Router();
     const csvController = require("../controllers/csv.controller.js");
     const uploadCourses = require("../middlewares/uploadCourses");
-    //const uploadSections = require("../middlewares/uploadSections");
+    const uploadSections = require("../middlewares/uploadSections");
     // Create 
     router.post("/", courses.create);
     // Retrieve all 
@@ -22,7 +22,7 @@ module.exports = app => {
     // Upload courses file
     router.post("/upload/courses", uploadCourses.single("file"), csvController.uploadCourses);
     // Upload sections file
-    //router.post("/upload/sections", uploadSections.single("file"), csvController.uploadSections);
+    router.post("/upload/sections", uploadSections.single("file"), csvController.uploadSections);
 
     app.use('/schedule-t1', router);
   };
