53a223bac8c409933ea3fcd79c6989a059ed7360
ThatGremlin <ben.johnson@eagles.oc.edu>
2022-11-03 16:22:58 -0500

    Saved changes that were made to dev.
    index.js - added routers
    specialList changes
    course.routes - schedule/ -> schedule/course/

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -2,19 +2,24 @@ module.exports = app => {
     const course = require("../controllers/course.controller.js");
     var router = require("express").Router();
     // Create 
-    router.post("/", course.create);
+    router.post("/course/", course.create);
     // Retrieve all 
-    router.get("/", course.findAll);
+    router.get("/course/", course.findAll);
     // Retrieve a single with id
-    router.get("/:id", course.findOne);
+    // Retrieve a single with course_number
+    router.get("/course/:course_number", course.findOne);
     // Retrieve a single with department
-    router.get("/dept/:dept", course.findDept);
+    router.get("/course/dept/:dept", course.findDept);
     // Retrieve a single with name
-    router.get("/name/:name", course.findName);
+    router.get("/course/name/:name", course.findName);
     // Update with id
-    router.put("/:id", course.update);
+    router.put("/course/:id", course.update);
     // Delete with id
-    router.delete("/:id", course.delete);
-    //Changed to schedule to build the api path to match the AWS server. Making router file unique by adding /courses
-    app.use('/schedule-t1/course', router);
+
+    router.delete("/course/:id", course.delete);
+    // Upload course file
+    // router.post("/upload/course", uploadCourses.single("file"), csvController.uploadCourses);
+    
+    app.use('/schedule-t1', router);
+
   };
